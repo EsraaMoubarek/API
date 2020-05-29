26dd2592abe424c13fce80d0036a0edb151edc47
sarasali757 <58108069+sarasali757@users.noreply.github.com>
2020-05-29 17:43:52 +0200

    request

---
+++ testwebapicore/Controllers/ClientController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testwebapicore.Models;
+using testwebapicore.Models.repo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testwebapicore.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        ClientRepo _db;
+        public ClientController(ClientRepo db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public ActionResult RegistrationAprtmnt(Client client)
+        {
+            if (client != null)
+            {
+                _db.RegisterApartment(client);
+
+                //CreatedAtction To Go To Home Or Logging First its important
+                return Created("successfully Created", client);
+            }
+            else
+            {
+                return Content("failed To Register Try Again");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult RegistrationRestrnt(Client client)
+        {
+            if (client != null)
+            {
+                _db.RegisterRestrnt(client);
+
+                //CreatedAtction To Go To Home Or Logging First its important
+                return Created("successfully Created", client);
+            }
+            else
+            {
+                return Content("failed To Register Try Again");
+            }
+
+        }
+        [HttpPost]
+        public IEnumerable<string> test()
+        {
+            return new string[] { "tessssst", "Jane Doe" };
+        }
+        public List<ClientCategory> GetCategory()
+        {
+            return _db.AllCategory();
+        }
+
+
+
+        //for test Skip it please
+        [HttpGet, Authorize]
+        //[Route("home")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "John Doe", "Jane Doe" };
+        }
+
+        ///******** Reqiest Part ********/// 
+        /// 
+        /// <summary>
+        /// ////////////// Requests List 
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult RequestsList(int id)
+        {
+            return Ok(_db.RequestsList(id));
+        }
+        /// 
+        /// <summary>
+        /// ////////////// New Request 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult StartNewRequest(int id)
+        {
+            bool flag = _db.StartNewRequest(id);
+            return Ok(flag);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult getClient(int id)
+        {
+            Client client = _db.GetClient(id);
+            return Ok(client);
+        }
+
+
+        [HttpGet]
+       // [Route("newRequest/getRegions")]
+        public ActionResult getRegions()
+        {
+            return Ok(_db.getRegions());
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult getAddresses(int id)
+        {
+            return Ok(_db.getAddresses(id));
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult getSchedules(int id)
+        {
+            return Ok(_db.getSchedules(id));
+        }
+
+        [HttpPost]
+      //  [Route("newRequest/AddNewRequest")]
+        public ActionResult AddNewRequest([FromBody] Request request)
+        {
+            _db.AddNewRequest(request);
+            return Ok(request);
+        }
+        /// <summary>
+        /// ////////////// Current Request 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetCurrentRequest(int id)
+        {
+            return Ok(_db.GetCurrentRequest(id));
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult DeleteRequest(int id)
+        {
+            return Ok(_db.DeleteRequest(id));
+        }
+    }
+}
--- testwebapicore/Models/repo/ClientRepo.cs
+++ testwebapicore/Models/repo/ClientRepo.cs
@@ -39,5 +39,136 @@ namespace testwebapicore.Models.repo
             List<ClientCategory> category = _db.ClientCategory.Select(a => new ClientCategory { Id = a.Id, Name = a.Name }).ToList();
             return category;
         }
+
+
+        /////Requests List
+        public List<Request> RequestsList(int id)
+        {
+
+            return _db.Request.Where(x => x.ClientId == id).Select(x => new Request()
+            {
+                Id = x.Id,
+                ApartmentNumber = x.ApartmentNumber,
+                ClientId = x.ClientId,
+                ScheduleId = x.ScheduleId,
+                BuildingNumber = x.BuildingNumber,
+                Points = x.Points,
+                OrgaincWeight = x.OrgaincWeight,
+                NonOrganicWeight = x.NonOrganicWeight,
+                AddressId = x.AddressId,
+                CollectorId = x.CollectorId,
+                Rate = x.Rate,
+                IsSeparated = x.IsSeparated,
+                Schedule = new Schedule()
+                {
+                    Time = x.Schedule.Time
+                }
+            }).ToList();
+        }
+        ///
+        /// new request
+        /// 
+        public bool StartNewRequest(int id)
+        {
+            bool flag = (_db.Request.Where(x => x.ClientId == id && x.Schedule.Time > DateTime.Now
+            && (x.OrgaincWeight <= 0 || x.NonOrganicWeight <= 0)).ToList()).Count > 0;
+
+            return !flag;
+        }
+        public Client GetClient(int id)
+        {
+            Client client = _db.Client.Select(x => new Client
+            {
+                Id = x.Id,
+                ClientName = x.ClientName,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Email = x.Email,
+                Mobile = x.Mobile,
+                AddressId = x.AddressId,
+                BuildingNumber = x.BuildingNumber,
+                ApartmentNumber = x.ApartmentNumber,
+                TotalPoints = x.TotalPoints,
+                CategoryId = x.CategoryId,
+                Address = new Address()
+                {
+                    Id = x.Address.Id,
+                    RegionId = x.Address.RegionId,
+                    StreetName = x.Address.StreetName
+                }
+            })
+                 .Where(x => x.Id == id).FirstOrDefault(); ;
+            return client;
+        }
+
+        public List<Region> getRegions()
+        {
+
+            return _db.Region.Select(x => new Region()
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToList();
+        }
+
+        public List<Address> getAddresses(int id)
+        {
+            List<Address> addresses = _db.Address.Select(x => new Address()
+            {
+                Id = x.Id,
+                RegionId = x.RegionId,
+                StreetName = x.StreetName
+            }).Where(a => a.RegionId == id).ToList();
+            return (addresses);
+        }
+
+        public List<Schedule> getSchedules(int id)
+        {
+
+            List<Schedule> schedules = _db.Schedule.Select(x => new Schedule()
+            {
+                Id = x.Id,
+                Time = x.Time,
+                DriverId = x.DriverId,
+                RegionId = x.RegionId
+            }).Where(a => a.RegionId == id).ToList();
+            return schedules;
+        }
+        public void AddNewRequest(Request request)
+        {
+            _db.Request.Add(request);
+            _db.SaveChanges();
+        }
+        public Request GetCurrentRequest(int id)
+        {
+            Request request = _db.Request.Select(x => new Request()
+            {
+                Id = x.Id,
+                ApartmentNumber = x.ApartmentNumber,
+                ClientId = x.ClientId,
+                ScheduleId = x.ScheduleId,
+                BuildingNumber = x.BuildingNumber,
+                Points = x.Points,
+                OrgaincWeight = x.OrgaincWeight,
+                NonOrganicWeight = x.NonOrganicWeight,
+                AddressId = x.AddressId,
+                CollectorId = x.CollectorId,
+                Rate = x.Rate,
+                IsSeparated = x.IsSeparated,
+                Schedule = new Schedule()
+                {
+                    Time = x.Schedule.Time
+                }
+            }).Where(x => x.ClientId == id).ToList().Last();
+            return request;
+        }
+
+        public Request DeleteRequest(int id)
+        {
+            Request request = _db.Request.Find(id);
+            _db.Request.Remove(request);
+            _db.SaveChanges();
+            return request;
+        }
     }
 }
